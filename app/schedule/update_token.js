964a6c9bd460d63b870e4d628be13ecd5d9009fb
leiyanggz <leiyanggz@gmail.com>
2018-09-24 16:18:47 -0700

    update_token debug

--- app/schedule/update_token.js
+++ app/schedule/update_token.js
@@ -14,32 +14,46 @@ module.exports = {
     // task 是真正定时任务执行时被运行的函数，第一个参数是一个匿名的 Context 实例
     * task(ctx) {
         //定时获取微信token
+        try {
+            const wechatConfig = ctx.app.config.ZLZD_WECHAT_CONFIG;
 
-        const wechatConfig = ctx.app.config.ZLZD_WECHAT_CONFIG;
+            let api = new WechatAPI(wechatConfig.appid, wechatConfig.appSecret);
 
-        let api = new WechatAPI(wechatConfig.appid, wechatConfig.appSecret);
-
-        let getToken = () => {
-            return new Promise((resolve, reject) => {
-                if (process.env.EGG_SERVER_ENV === 'prod') {
-                    api.getLatestToken(function(err, token) {
-                        resolve(token);
+            let getToken = () => {
+                return new Promise((resolve, reject) => {
+                    api.getLatestToken(function (err, token) {
+                        if(!err) {
+                            resolve(token);
+                        }
+                        else{
+                            ctx.logger.error(`
+                                ================== WechatAPI getLatestToken Error ===================
+                                Error: ${err}
+                            `);
+                        }
                     });
-                } else {
-                    resolve({ "access_token": "JSi5TouUaDR-n4n1Oc7gr0uuDz2LlF8X3vfqEuIxE7pYduYnB9WAh8FhidsAuD8O_vQeR4QY7NWsL8LQCY-oQVeIwGspbL5UaF_d6UP5_Q2FX0_FCw-uLcFzsydB8tN_SXXeAFAHMM", "expires_in": 7200 });
-                }
-            });
-        };
+                });
+            };
 
-        let token = yield getToken();
-        if (token) {
-            console.log(new Date().toLocaleTimeString(), ":create wechatAPI", wechatConfig.appid, wechatConfig.appSecret);
-            ctx.app.messenger.sendToApp('newWechatToken',token);
+            let token = yield getToken();
+            if (token) {
+                console.log(new Date().toLocaleTimeString(), ":create wechatAPI", wechatConfig.appid, wechatConfig.appSecret);
+                ctx.app.messenger.sendToApp('newWechatToken', token);
+
+                // menu
+                api.createMenu(ctx.app.CONSTANT.QTC_WECHAT.MENU, function (err) {
+                    ctx.logger.error(`
+                        ================== WechatAPI createMenu Error ===================
+                        Error: ${err}
+                    `);
+                })
+            }
         }
-
-        // menu
-        api.createMenu(ctx.app.CONSTANT.QTC_WECHAT.MENU, function (err) {
-            throw err;
-        })
+        catch (e) {
+            ctx.logger.error(`
+                ================== update_token schedule Error ===================
+                Error: ${e}
+            `);
+        }
     }
 };
