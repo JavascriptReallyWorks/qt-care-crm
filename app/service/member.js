fdf99451ff57e0df5a189b67f6e19931d4ec27f9
leiyanggz <leiyanggz@gmail.com>
2019-04-12 11:15:28 -0700

    修改保险承保判断标准

--- app/service/member.js
+++ app/service/member.js
@@ -19,7 +19,7 @@ module.exports = app => {
             birth: birth ? birth.split('T')[0] : '',
             orders: orders.map(order => ({
               ...order,
-              order_status: order.order_status === '承保' ? 1 : 4,
+              order_status: order.order_status === '终止' ? 4 : 1,
               insurance_start_date: order.insurance_start_date ? order.insurance_start_date.split('T')[0] : '',
               insurance_end_date: order.insurance_end_date ? order.insurance_end_date.split('T')[0] : '',
             })),
